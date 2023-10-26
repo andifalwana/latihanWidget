944811aaa46c633bd9efbe7781f0a93ff668b89a
andifalwana <andifalwana09@gmail.com>
2023-10-26 09:41:24 +0700

    latihan widget

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Latihan widget")),
+        body: Container(
+          color: Colors.black26,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                height: 200,
+                color: Colors.yellow,
+              ),
+              Container(
+                color: Colors.white,
+                width: double.infinity,
+                child: Text("Belajar Flutter - STT Bandung", textAlign: TextAlign.center,),
+              ),
+              IntrinsicHeight(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(child: Container(
+                      color: Colors.orange,
+                      height: 150,
+                    )),
+                    Expanded(child: Container(color: Colors.green,)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
